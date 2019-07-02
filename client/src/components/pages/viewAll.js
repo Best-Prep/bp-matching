f495bcba99e3020e439a6bb2e6cb02cdacbfa095
Krohn, Lillian E <lillian.krohn@optum.com>
2019-07-02 17:28:24 -0500

    Data display on classroom cards from json is working

--- client/src/components/pages/viewAll.js
+++ client/src/components/pages/viewAll.js
@@ -1,13 +1,23 @@
 import React, {useState} from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
-import ClassroomCard from '../organisms/classroom_card/ClassroomCard'
-const view = () => {
+import ClassroomCard from '../organisms/classroom_card/ClassroomCard';
+
+let data = require('../../CareerDay.json') 
+
+const useStyles = makeStyles(theme => ({
+    root: {
+      flexGrow: 1,
+    }
+  }));
+
+const ViewAll = (props) => {
+    const [classroomData, setclassroomData] = useState(data)
     return (
-        <div>
-           <ClassroomCard/>
-        </div>
+            <ClassroomCard teacherName={data.teacher_first + " " + data.teacher_last} />
+            
+           
     )
 }
 
-export default view
+export default ViewAll
