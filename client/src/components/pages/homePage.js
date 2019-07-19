bff95011beb71144650ff87b813fa9781b3a0a92
Krohn, Lillian E <lillian.krohn@optum.com>
2019-07-19 10:00:07 -0500

    Added text field for login

--- client/src/components/pages/homePage.js
+++ client/src/components/pages/homePage.js
@@ -5,12 +5,10 @@ import Typography from "@material-ui/core/Typography";
 import Grid from '@material-ui/core/Grid';
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
-import clsx from 'clsx';
 import Button from '@material-ui/core/Button';
-import KeyboardVoiceIcon from '@material-ui/icons/KeyboardVoice';
 import Icon from '@material-ui/core/Icon';
-import SaveIcon from '@material-ui/icons/Save';
-import IconButton from '@material-ui/core/IconButton';
+import TextField from '@material-ui/core/TextField'
+
 
 
 
@@ -47,7 +45,18 @@ const UseStyles = makeStyles(theme => ({
       },
     iconSmall: {
         fontSize: 20,
-      }
+      },
+    textField: {
+        marginLeft: theme.spacing(1),
+        marginRight: theme.spacing(1),
+        width: 200,
+    },
+    dense: {
+        marginTop: 19,
+    },
+    menu: {
+        width: 200,
+    }
   }));
 
  const homePage = () => {
@@ -62,8 +71,16 @@ const UseStyles = makeStyles(theme => ({
                 <Paper className={classes.fixedHeightPaper}>
                
                     <img src={require('./bestprepLogo.png')} alt="bestPrep" style={{height: 180, width: 380, paddingRight: 50}}/>
-
-                    <div> <form><p>Login: <input type="text"></input></p></form></div>
+{/* 
+                    <div> <form><p>Login: <input type="text"></input></p></form></div> */}
+                    <form className={classes.container} noValidate autoComplete="off">
+                    <TextField
+                        id="standard-name"
+                        label="Name"
+                        className={classes.textField}
+                        margin="normal"
+                    />
+                    </form>
                     <div><Button variant="contained" color="primary" className={classes.button}>
                     Submit
                     <Icon className={classes.rightIcon}> <i class="material-icons">
