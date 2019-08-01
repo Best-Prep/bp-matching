4489792d60d4b76f3b2bcec6468fa1964a7d8f69
Oster <toster@ms.ds.uhc.com>
2019-08-01 10:02:00 -0500

    Fixed careerDayDashboard

--- client/src/components/pages/careerDayDashboard.js
+++ client/src/components/pages/careerDayDashboard.js
@@ -1,8 +1,8 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 //Utilities
 import { makeStyles } from '@material-ui/core/styles';
-
+import axios from 'axios';
 //Material-UI
 import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
@@ -15,33 +15,11 @@ import CareerDayStats from '../atoms/careerDayStats/CareerDayStats';
 import CareerDayTable from '../atoms/careerDayTable/CareerDayTable';
 import Header from '../organisms/header/header';
 
-let careerDay = require('../../careerDay.json')
-let regClasses = require('../../registeringClasses.json')
 /* 
     Material-UI's dashboard example was used for reference, no need to reinvent the wheel
     https://github.com/mui-org/material-ui/tree/master/docs/src/pages/getting-started/page-layout-examples/dashboard
     */
-let rows = []
-let totalStudents = 0;
-const prepData = () =>{
-    
-    careerDay.schools.forEach(school =>{
-        let studentCount = 0
-        let regClassNum = regClasses.filter(regClass => {
-            if (regClass.school.id === school.id && regClass.careerDayId === careerDay.id){
-                studentCount += regClass.students.length
-            }
-            return (regClass.school.id === school.id && regClass.careerDayId === careerDay.id)
-        }).length
-        rows.push({
-            "name": school.name,
-            "registeringClasses": regClassNum,
-            "students": studentCount
-        })
-        totalStudents += studentCount;
-    })
-    return rows;
-}
+
 const UseStyles = makeStyles(theme => ({
     root: {
       display: 'flex',
@@ -65,40 +43,81 @@ const UseStyles = makeStyles(theme => ({
     }
   }));
 
-const careerDayDashboard = () => {
-    prepData()
+const CareerDayDashboard = () => {
+    let rows = []
+    let totalStudents = 0;
+    const prepData = () =>{
+        
+        careerDay.schools.forEach(school =>{
+            let studentCount = 0
+            let regClassNum = regClasses.filter(regClass => {
+                if (regClass.school.id === school.id && regClass.careerDayId === careerDay.id){
+                    studentCount += regClass.students.length
+                }
+                return (regClass.school.id === school.id && regClass.careerDayId === careerDay.id)
+            }).length
+            rows.push({
+                "name": school.name,
+                "registeringClasses": regClassNum,
+                "students": studentCount
+            })
+            totalStudents += studentCount;
+        })
+        return rows;
+    }
+    const [careerDayDate, setCareerDayDate] = useState("")
+    const [regClasses, setRegClasses] = useState()
+    const [careerDay, setCareerDay] = useState()
+    const handleDateChange = (event) => {
+        axios.post('/api/careerDay.getDashboard', 
+          {
+            "careerDayDate": event.target.value
+          },{headers: {
+            'Content-Type': 'application/json',
+          }}
+        ).then(function (response) {
+          setRegClasses(response.data.classrooms);
+          setCareerDay(response.data.careerDay);
+          prepData()
+        })
+      }
     const classes = UseStyles();
     return (
     <div>
-    <div style={{fontWeight: 'bold'}}>
-      <Header linkTo='/' headName='BestPrep' style={{fontWeight: 'bold'}}/>
-      </div>
-        <Container maxWidth="lg" className={classes.container}>
-            <Grid container spacing={3}>
-                {/* Chart */}
-                <Grid item xs={12} md={8} lg={9}>
-                    <Paper className={classes.fixedHeightPaper}>
-                        <PreferenceBarChart/>
-                    </Paper>
-                </Grid>
-                <Grid item xs={12} md={4} lg={3}>
-                    <Paper className={classes.fixedHeightPaper}>
-                        <CareerDayStats
-                            totalRegistered={careerDay.schools.length}
-                            totalStudents={totalStudents}
-                            totalSubjects={careerDay.subjects.length}
-                        />
-                    </Paper>
-                </Grid>
-                <Grid item xs={12}>
-                    <Paper className={classes.Paper}>
-                        <CareerDayTable rows={rows}/>
-                    </Paper>
-                </Grid>
-            </Grid>
-        </Container>
+    {careerDay === null ? (
+        <p>asdfasdf<p>
+    ):(
+        <div style={{fontWeight: 'bold'}}>
+        <Header linkTo='/' headName='BestPrep' style={{fontWeight: 'bold'}}/>
         </div>
+          <Container maxWidth="lg" className={classes.container}>
+              <Grid container spacing={3}>
+                  {/* Chart */}
+                  <Grid item xs={12} md={8} lg={9}>
+                      <Paper className={classes.fixedHeightPaper}>
+                          <PreferenceBarChart/>
+                      </Paper>
+                  </Grid>
+                  <Grid item xs={12} md={4} lg={3}>
+                      <Paper className={classes.fixedHeightPaper}>
+                          <CareerDayStats
+                              totalRegistered={careerDay.schools.length}
+                              totalStudents={totalStudents}
+                              totalSubjects={careerDay.subjects.length}
+                          />
+                      </Paper>
+                  </Grid>
+                  <Grid item xs={12}>
+                      <Paper className={classes.Paper}>
+                          <CareerDayTable rows={rows}/>
+                      </Paper>
+                  </Grid>
+              </Grid>
+          </Container>
+          </div>
+    )}
+    
     )
 }
 
-export default careerDayDashboard
+export default CareerDayDashboard
